1bee9085707a08db95a07f17259328f7bd68b957
wangning <wangning@51signing.com>
2019-09-18 10:02:02 +0800

    链接 轮询

--- pages/recognition/recognition.js
+++ pages/recognition/recognition.js
@@ -1,106 +1,127 @@
 // pages/recognition/recognition.js
-import upng from '../../UPNG.js'
+// import upng from '../../UPNG.js'
 Page({
-  data: {
-    height: '360',
-    width: '20',
-    status: false,
-    scanStatus: 'none',
-    msg: "请点击识别图片",
-    src: '',
-    listener: null,
-    isReuqest: false,
-    canvasWidth: '10',
-    canvasHeight:'10',
-    cameraCtx:''
-  },
-
-  onLoad: function (options) {
-    this.ctx = wx.createCameraContext();
-
-    wx.getSystemInfo({
-      success: res => {
-        this.setData({ height: res.windowHeight * 0.8, width: res.windowWidth});
-      }
-    });
-    var content = wx.createCameraContext()
-    console.log(content)
-  },
-
-  stopScan: function () {
-    this.setData({ scanStatus: 'none' });
-  },
-
-  onShow: function () {
-    this.setData({ msg: '请点击识别图片' });
-  },
-
-  error: function (e) {
-    this.setData({ msg: '打开摄像头失败，请点击“立即体验' });
-  },
-
-
-  searchPhoto: function(imageBase64) {
-    let that = this;
-    wx.request({
-      url: 'https://cn1-crs.easyar.com:8443/search', 
-      data: {
-        image: imageBase64
-      },
-      header: {
-        'Authorization': 'jfI9YqnhofaI1mqA80TpQ5QCAp5b8McpOYVjkBpBmLQaN/w/SRRv0UYFK8pp3IasKHkAif/+piSxsz1VJoCscw==',
-        'content-type': 'application/json' // 默认值
-      },
-      method: 'POST',
-      success(res) {
-        console.log(res)
-        that.setData({isReuqest: false});
-        if (res.data.statusCode == 0) {
-          that.listener.stop();
-          that.setData({ msg: '识别成功'});
-          setTimeout(() => {
-            console.info('go to webar');
-            wx.navigateTo({
-              url: '../show/show'
-            });
-          }, 500);
+    data: {
+      height: '360',
+      width: '20',
+      status: false,
+      scanStatus: 'none',
+      msg: "请点击识别图片",
+      src: '',
+      timer:null
+    },
+  
+    onLoad: function (options) {
+      this.ctx = wx.createCameraContext();
+  
+      wx.getSystemInfo({
+        success: res => {
+          this.setData({ height: res.windowHeight * 0.8, width: res.windowWidth});
         }
-      },
-
-      fail(err) {
-        console.log(err)
-        that.status = false;
-        that.setData({ msg: '识别失败，请点击重试', isReuqest: false});
-      }
-    })
-  },
-
-  transformArrayBufferToBase64: function (frame) {
-    const data = new Uint8ClampedArray(frame.data);
-    this.setData({isReuqest: true});
-    let pngData = upng.encode([frame.data], frame.width, frame.height, 16, 0);
-    let base64 = wx.arrayBufferToBase64(pngData);
-    this.searchPhoto(base64)
-  },
-
-  takePhoto: function (e) {
-    if (this.status) return;
-    this.status = true;
-    var context = wx.createCameraContext()
-    console.log(context)
-    this.listener = context.onCameraFrame((frame) => {
-      if(!this.data.isReuqest) {
-        this.transformArrayBufferToBase64(frame);
-      }
-    });
-    this.listener.start({
-      success: () => {
-        console.log('监听成功')
-      },
-      fail: (err) => {
-        console.log('监听失败')
-        console.log(err)
-      }
-    })
-  }
-})
+      });
+    },
+  
+    stopScan: function () {
+      this.setData({ scanStatus: 'none' });
+    },
+  
+    onShow: function () {
+      this.setData({ msg: '开始识别' });
+    },
+  
+    error: function (e) {
+      this.setData({ msg: '打开摄像头失败，请点击“立即体验' });
+    },
+  
+  
+    urlTobase64(imgPath) {
+      let that = this;
+  
+      wx.getFileSystemManager().readFile({
+        filePath: imgPath, //选择图片返回的相对路径
+        encoding: 'base64', //编码格式
+        success: res => { //成功的回调
+          // console.log('data:image/png;base64,' + res.data)
+          that.searchPhotp(res.data)
+        }
+      })
+  
+  
+      // let canvas = wx.createCanvasContext('firstCanvas')
+      // // 1. 绘制图片至canvas
+      // canvas.drawImage(imgPath, 0, 0, this.data.width, this.data.height);
+      // // 绘制完成后执行回调，API 1.7.0
+      // canvas.draw(false, () => {
+      //   // 2. 获取图像数据， API 1.9.0
+      //   wx.canvasGetImageData({
+      //     canvasId: 'firstCanvas',
+      //     x: 0,
+      //     y: 0,
+      //     width: this.data.width,
+      //     height: this.data.height,
+      //     success(res) {
+      //       // console.log(res)
+      //       // 3. png编码
+      //      let pngData = upng.encode([res.data.buffer], res.width, res.height, 1, 0)
+      //       // 4. base64编码
+      //      let base64 = wx.arrayBufferToBase64(pngData)
+      //       that.searchPhotp(base64)
+      //     }
+      //   })
+      // })
+    },
+  
+    searchPhotp: function(imageBase64) {
+      let that = this;
+      wx.request({
+        url: 'http://47.96.163.82/api/ar-demo;', 
+        data: {
+          img: imageBase64
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        method: 'POST',
+        success(res) {
+          if (res.data.statusCode == 0) {
+            clearInterval(that.data.timer)
+            that.setData({ msg: '识别成功'});
+            setTimeout(() => {
+              console.info('go to webar');
+              wx.navigateTo({
+                url: '../show/show'
+              });
+            }, 500);
+          } else {
+            that.status = false;
+            clearInterval(that.data.timer)
+            that.setData({ msg: '识别失败，请点击重试'});
+          }
+        },
+  
+        fail(err) {
+          console.log(err)
+          that.status = false;
+          that.setData({ msg: '识别失败，请点击重试'});
+        }
+      })
+    },
+  
+    takePhoto: function (e) {
+      if (this.status) return;
+      this.status = true;
+      this.data.timer = setInterval(()=>{
+        this.ctx.takePhoto({
+          quality: 'low',
+          success: res => {
+            this.setData({ msg: '识别中...', src: res.tempImagePath });
+            this.urlTobase64(res.tempImagePath);
+          },
+          fail: err => {
+            this.stopScan();
+            this.setData({ msg: '未识别到目标' });
+          }
+        });
+      },3000)
+    }
+  })
